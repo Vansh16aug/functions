2f5c9ce65ac6d90cf96d6e62ed63268a435ba69d
Vansh kumar <kumarvansh16aug@gmail.com>
2023-06-29 21:04:10 +0530

    Add files via upload

---
+++ functions1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+void printName(string name){
+    cout<<"Hello World "<<name<<endl;
+}
+int main()
+{
+    string name;
+    cout<<"Enter your name: ";
+    cin>>name;
+    printName(name);
+    return 0;
+}
